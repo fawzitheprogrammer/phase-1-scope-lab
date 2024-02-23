71cde3461b22b4b2921adcddba7551ed47baa955
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-24 00:59:15 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Set bestCustomer without declaring it with var, let, or const to make it global
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Attempting to change a constant should throw an error
+const leastFavoriteCustomer = 'initial value';
+function changeLeastFavoriteCustomer() {
+    // This will indeed throw a TypeError, which is expected and correct
+    leastFavoriteCustomer = 'new value';
+}
